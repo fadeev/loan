9e7e02ed03aa1b41a7ad977f499a994ac829a46e
Denis Fadeev <denis@fadeev.org>
2021-10-05 12:27:57 +0500

    errors: fix index

--- x/loan/types/errors.go
+++ x/loan/types/errors.go
@@ -9,5 +9,5 @@ import (
 // x/loan module sentinel errors
 var (
 	ErrWrongLoanState = sdkerrors.Register(ModuleName, 1, "wrong loan state")
-	ErrDeadline       = sdkerrors.Register(ModuleName, 1, "deadline")
+	ErrDeadline       = sdkerrors.Register(ModuleName, 2, "deadline")
 )
